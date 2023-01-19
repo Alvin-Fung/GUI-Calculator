c743c96f8e3f4e67a4c18d33938d9bc9acf45f9d
Alvin-Fung <alvin_fung9@outlook.com>
2023-01-19 17:41:15 +0000

    FrameHandler and StandardPage class created, Buttons have been modified to utilize the CalculatorButton class
    It doesn't work right now, but it's getting closer.

--- GUI Calculator.py
+++ GUI Calculator.py
@@ -11,9 +11,9 @@ def add_to_operation(symbol):
     global operation #allows to be manipulated inside a function
     
     operation += str(symbol)
-    window_result.delete(1.0 , "end") #first index position, last index position
+    StandardPage.window_result.delete(1.0 , "end") #first index position, last index position
     #The 1 is the first line and the 0 is before the first character, "end" is end of text
-    window_result.insert(1.0, operation) #index position, string
+    StandardPage.window_result.insert(1.0, operation) #index position, string
 
 def evaluate_operation():
     global operation
@@ -22,142 +22,119 @@ def evaluate_operation():
         result = str(eval(operation))
         # print(result)
         operation = ""
-        window_result.delete(1.0, "end") 
-        window_result.insert(1.0, result) 
+        StandardPage.window_result.delete(1.0, "end") 
+        StandardPage.window_result.insert(1.0, result) 
     
     except:
         clear_operation()
-        window_result.insert(1.0, "Incorrect Syntax")
+        StandardPage.window_result.insert(1.0, "Incorrect Syntax")
         pass
 
 def clear_operation():
     global operation
     operation = ""
-    window_result.delete(1.0, "end")
-
-#Frames - This is required in order to switch between two variations of the calculator for different operations
-
-# class Stack(tk.frame): #This handles the stack order of the frames
-    
-#     def __init__(self, *args, **kwargs):
-#         window.__init__(self, *args, **kwargs)
-
-#         #The container is where the frames will be stacked
-#         #on top of each other, then the one want present
-#         #will be raised above the others
-#         container = tk.window(self)
-#         container.pack()
-
-#         self.window = {}
-#         for F in (Standard, Scientific):
-#             page_name = F.__name__
-#             window = F(parent = container, controller = self)
-#             self.frames[page_name] = window
-            
-#             window.geometry("460x435")
-#             window.minsize(460,435)
-#             window.maxsize(460,435)
-            
-#         self.show_window("Standard")
-    
-#     def show_window(self, page_name):
-#         window = self.windows[page_name]
-#         window.tkraise()
-
-# #THIS NEEDS MORE WORKING ON.
-
-# class Standard(tk.frame):
-#     pass
-
-# class Scientific(tk.Frame):
-#     pass
-
+    StandardPage.window_result.delete(1.0, "end")
 
 # Window Management
-window = tk.Tk() #This is the top level widget of Tk - represents mostly the main window of an application.
-window.title("Calculator MK 6")
-window.configure(background = "grey15")
-window.geometry("460x435")
-window.minsize(460,435)
-window.maxsize(460,435)
-window_result = tk.Text(window, height=2, width=16, font=("Roboto Mono", 24)) #Establish the result's dimensions.
-window_result.grid(columnspan = 5)
+#Frames - This is required in order to switch between two variations of the calculator for different operations
 
+class FrameHandler(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(StandardPage)
+
+    def switch_frame(self, frame_class):
+        "Destroys the current frame and replaces it with a new one."
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+class StandardPage(tk.Frame):
+     
+     def __init__(self, parent, title, configure, size ):
+        #This is the top level widget of Tk - represents mostly the main window of an application.
+        super().__init__(parent, title=title, configure = configure, size=size,) 
+        self.title("Calculator MK6")
+        self.configure(background = "grey15")
+        self.size(460,435)
+        self.maxsize(460,435)
+        self.attributes = {
+
+        }
+
+        window_result = tk.Text(self, height=2, width=16, font=("Roboto Mono", 24)) #Establish the result's dimensions.
+        window_result.grid(columnspan = 5)
 
 #Buttons
-# switch = Button(window, text = "Mode", command = lambda: pass)
-
-class my_button(tk.Button):
+class CalculatorButton(tk.Button):
     
-    def __init__(self, master, **kwargs):
-        super().__init__(master =master, **kwargs) #Inheritance
-        self.attributes = {"bg":'grey25', "width":'5'}
+    def __init__(self, parent, text, function):
+        super().__init__(parent, text=text, command=function) #Inheritance
+        self.attributes = {
+            "bg":'grey25', "font": ('Roboto Mono', 24),
+            "width":'5',"padx":'5', "pady": '5'}
         self.config(self.attributes)
 
-btn_1 = my_button(window,text ="1", command = lambda: add_to_operation(1))
-btn_1.grid(row = 2, column  = 1, padx = 5, pady = 5)
+btn_1 = CalculatorButton(StandardPage, "1", lambda: add_to_operation(1))
+btn_1.grid(row = 2, column  = 1,)
 
-# btn_2 = my_button(window,text ="2", command = lambda: add_to_operation(2))
-# btn_2.grid(row = 2, column = 2)
+btn_2 = CalculatorButton(StandardPage, "2", lambda: add_to_operation(2))
+btn_2.grid(row = 2, column = 2)
 
-# btn_3 = my_button(window, text ="3", command = lambda: add_to_operation(3))
-# btn_3.grid(row = 2, column = 3)
+btn_3 = CalculatorButton(StandardPage, "3", lambda: add_to_operation(3))
+btn_3.grid(row = 2, column = 3)
 
-# btn_1 = Button(window, text ="1", command = lambda: add_to_operation(1), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_1.grid(row = 2, column = 1, padx = 5, pady = 5)
+btn_4 = CalculatorButton(StandardPage, "4", lambda: add_to_operation(4))
+btn_4.grid(row = 3, column = 1)
 
-btn_2 = Button(window, text ="2", command = lambda: add_to_operation(2), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-btn_2.grid(row = 2, column = 2, padx = 5, pady = 5)
+btn_5 = CalculatorButton(StandardPage, "5", lambda: add_to_operation(5))
+btn_5.grid(row = 3, column = 2)
 
-btn_3 = Button(window, text ="3", command = lambda: add_to_operation(3), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-btn_3.grid(row = 2, column = 3, padx = 5, pady = 5)
+btn_6 = CalculatorButton(StandardPage, "6", lambda: add_to_operation(6))
+btn_6.grid(row = 3, column = 3)
 
-# btn_4 = Button(window, text ="4", command = lambda: add_to_operation(4), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_4.grid(row = 3, column = 1, padx = 5, pady = 5)
+btn_7 = CalculatorButton(StandardPage, "7", lambda: add_to_operation(7))
+btn_7.grid(row = 4, column = 1)
 
-# btn_5 = Button(window, text ="5", command = lambda: add_to_operation(5), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_5.grid(row = 3, column = 2, padx = 5, pady = 5)
+btn_8 = CalculatorButton(StandardPage, "8", lambda: add_to_operation(8))
+btn_8.grid(row = 4, column = 2)
 
-# btn_6 = Button(window, text ="6", command = lambda: add_to_operation(6), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_6.grid(row = 3, column = 3, padx = 5, pady = 5)
+btn_9 = CalculatorButton(StandardPage, "9", lambda: add_to_operation(9))
+btn_9.grid(row = 4, column = 3)
 
-# btn_7 = Button(window, text ="7", command = lambda: add_to_operation(7), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_7.grid(row = 4, column = 1, padx = 5, pady = 5)
+btn_0 = CalculatorButton(StandardPage, "0", lambda: add_to_operation(0))
+btn_0.grid(row = 5, column = 2)
 
-# btn_8 = Button(window, text ="8", command = lambda: add_to_operation(8), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_8.grid(row = 4, column = 2, padx = 5, pady = 5)
+add = CalculatorButton(StandardPage, "+", lambda: add_to_operation("+"))
+add.grid(row = 2, column = 4)
 
-# btn_9 = Button(window, text ="9", command = lambda: add_to_operation(9), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_9.grid(row = 4, column = 3, padx = 5, pady = 5)
+minus = CalculatorButton(StandardPage, "-", lambda: add_to_operation("-"))
+minus.grid(row = 3, column = 4)
 
-# btn_0 = Button(window, text ="0", command = lambda: add_to_operation(0), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# btn_0.grid(row = 5, column = 2, padx = 5, pady = 5)
+mul = CalculatorButton(StandardPage, "*", lambda: add_to_operation("*"))
+mul.grid(row = 4, column = 4)
 
-# add = Button(window, text ="+", command = lambda: add_to_operation("+"), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# add.grid(row = 2, column = 4, padx = 5, pady = 5)
+div = CalculatorButton(StandardPage, "/", lambda: add_to_operation("/"))
+div.grid(row = 5, column = 4)
 
-# minus = Button(window, text = "-", command = lambda: add_to_operation("-"), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# minus.grid(row = 3, column = 4, padx = 5, pady = 5)
+l_bracket = CalculatorButton(StandardPage, "(", lambda: add_to_operation("("))
+l_bracket.grid(row = 5, column = 1)
 
-# mul = Button(window, text = "*", command = lambda: add_to_operation("*"), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# mul.grid(row = 4, column = 4, padx = 5, pady = 5)
+r_bracket = CalculatorButton(StandardPage, ")", lambda: add_to_operation(")"))
+r_bracket.grid(row = 5, column = 3)
 
-# div = Button(window, text = "/", command = lambda: add_to_operation("/"), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# div.grid(row = 5, column = 4, padx = 5, pady = 5)
+decimal = CalculatorButton(StandardPage,".", lambda: add_to_operation("."))
+decimal.grid(row = 6, column = 1, columnspan = 2)
 
-# l_bracket = Button(window, text = "(", command = lambda: add_to_operation("("), width = 5,font = ("Roboto Mono", 24), bg = "grey25")
-# l_bracket.grid(row = 5, column = 1, padx = 5, pady = 5)
+clear = CalculatorButton(StandardPage, "C", clear_operation)
+clear.grid(row = 6, column = 2, columnspan = 2)
 
-# r_bracket = Button(window, text = ")", command = lambda: add_to_operation(")"), width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# r_bracket.grid(row = 5, column = 3, padx = 5, pady = 5)
+equals = CalculatorButton(StandardPage, "=", evaluate_operation)
+equals.grid(row = 6, column = 3, columnspan = 2)
 
-# decimal = Button(window, text = ".", command = lambda: add_to_operation("."), width = 5, font = ("Roboto Mons", 24), bg = "grey25")
-# decimal.grid(row = 6, column = 1, columnspan = 2, padx = 5, pady = 5)
-
-# clear = Button(window, text = "C", command = clear_operation, width = 5,font = ("Roboto Mono", 24), bg = "grey25")
-# clear.grid(row = 6, column = 2, columnspan = 2, padx = 5, pady = 5)
-
-# equals = Button(window, text = "=", command = evaluate_operation, width = 5, font = ("Roboto Mono", 24), bg = "grey25")
-# equals.grid(row = 6, column = 3, columnspan = 2, padx = 5, pady = 5)
-
-window.mainloop()
+if __name__ == "__main__":
+    app = FrameHandler()
+    app.mainloop()
